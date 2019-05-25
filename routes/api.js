791136e71aec87b897f127309ba25bad5daaefff
Douglas Ray <dpr980@gmail.com>
2019-05-25 21:26:30 +0100

    added tests for PUT requests, implemented endpoint logic, tests passing

--- routes/api.js
+++ routes/api.js
@@ -9,41 +9,72 @@
 'use strict';
 
 module.exports = function(app, db) {
-	app
-		.route('/api/issues/:project')
-		.get(function(req, res) {
-			const project = req.params.project;
-			console.log(project);
-		})
+  app
+    .route('/api/issues/:project')
+    .get(function(req, res) {
+      const project = req.params.project;
+      console.log(project);
+    })
 
-		.post(function(req, res, next) {
-			const timestamp = new Date();
-			const project = req.params.project;
-			const { created_by, issue_text, issue_title } = req.body;
-			const issue = {
-				created_on: timestamp,
-				updated_on: timestamp,
-				open: true,
-				...req.body
-			};
-			if (!created_by || !issue_text || !issue_title) {
-				return res.json({ error: 'You have not completed the form.' });
-			} else {
-				db.collection(project).insertOne(issue, (err, doc) => {
-					if (err) {
-						res.json({ msg: 'There was an error adding your issue', err });
-					} else {
-						res.json(issue);
-					}
-				});
-			}
-		})
+    .post(function(req, res, next) {
+      const timestamp = new Date();
+      const { project } = req.params;
+      const { created_by, issue_text, issue_title } = req.body;
+      const issue = {
+        created_on: timestamp,
+        updated_on: timestamp,
+        open: true,
+        ...req.body
+      };
+      if (!created_by || !issue_text || !issue_title) {
+        return res.json({ error: 'You have not completed the form.' });
+      } else {
+        db.collection(project).insertOne(issue, (err, doc) => {
+          if (err) {
+            res.json({ msg: 'There was an error adding your issue', err });
+          } else {
+            res.json(issue);
+          }
+        });
+      }
+    })
 
-		.put(function(req, res) {
-			const project = req.params.project;
-		})
+    .put(function(req, res) {
+      const project = req.params.project;
+      const {
+        _id,
+        issue_title,
+        issue_text,
+        created_by,
+        assigned_to,
+        status_text,
+        open
+      } = req.body;
+      if (
+        !issue_title &&
+        !issue_text &&
+        !created_by &&
+        !assigned_to &&
+        !status_text &&
+        typeof open === 'undefined'
+      ) {
+        res.send('no updated field sent');
+      } else {
+        db.collection(project).findOneAndUpdate(
+          { _id },
+          { $set: { ...req.body } },
+          (err, doc) => {
+            if (err) {
+              res.json(`Could not update ${_id}`);
+            } else {
+              res.send('successfully updated');
+            }
+          }
+        );
+      }
+    })
 
-		.delete(function(req, res) {
-			const project = req.params.project;
-		});
+    .delete(function(req, res) {
+      const project = req.params.project;
+    });
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -14,81 +14,121 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-	suite('POST /api/issues/{project} => object with issue data', function() {
-		test('Every field filled in', function(done) {
-			chai
-				.request(server)
-				.post('/api/issues/test')
-				.send({
-					issue_title: 'Title',
-					issue_text: 'text',
-					created_by: 'Bilbo',
-					assigned_to: 'Chai and Mocha',
-					status_text: 'In QA'
-				})
-				.end(function(err, res) {
-					assert.equal(res.status, 200);
-					assert.equal(res.type, 'application/json', 'Response should be json');
-					assert.isTrue(res.body.open, 'Issue status exists');
-					assert.isDefined(res.body._id, 'Issue _id exists');
-					assert.isDefined(res.body.created_on, 'Issue timestamp exists');
-					assert.equal(res.body.created_on, res.body.updated_on);
-					assert.equal(res.body.assigned_to, 'Chai and Mocha');
-					assert.equal(res.body.status_text, 'In QA');
-					done();
-				});
-		});
+  suite('POST /api/issues/{project} => object with issue data', function() {
+    test('Every field filled in', function(done) {
+      chai
+        .request(server)
+        .post('/api/issues/test')
+        .send({
+          issue_title: 'Title',
+          issue_text: 'text',
+          created_by: 'Bilbo',
+          assigned_to: 'Chai and Mocha',
+          status_text: 'In QA'
+        })
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json', 'Response should be json');
+          assert.isTrue(res.body.open, 'Issue status exists');
+          assert.isDefined(res.body._id, 'Issue _id exists');
+          assert.isDefined(res.body.created_on, 'Issue timestamp exists');
+          assert.equal(res.body.created_on, res.body.updated_on);
+          assert.equal(res.body.assigned_to, 'Chai and Mocha');
+          assert.equal(res.body.status_text, 'In QA');
+          done();
+        });
+    });
 
-		test('Required fields filled in', function(done) {
-			chai
-				.request(server)
-				.post('/api/issues/test')
-				.send({
-					issue_title: 'some title',
-					issue_text: 'some text',
-					created_by: 'Bilbo'
-				})
-				.end(function(err, res) {
-					assert.equal(res.status, 200);
-					assert.equal(res.type, 'application/json', 'Response should be json');
-					assert.equal(res.body.created_by, 'Bilbo');
-					assert.equal(res.body.issue_title, 'some title');
-					assert.equal(res.body.issue_text, 'some text');
-					done();
-				});
-		});
+    test('Required fields filled in', function(done) {
+      chai
+        .request(server)
+        .post('/api/issues/test')
+        .send({
+          issue_title: 'some title',
+          issue_text: 'some text',
+          created_by: 'Bilbo'
+        })
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json', 'Response should be json');
+          assert.equal(res.body.created_by, 'Bilbo');
+          assert.equal(res.body.issue_title, 'some title');
+          assert.equal(res.body.issue_text, 'some text');
+          done();
+        });
+    });
 
-		test('Missing required fields', function(done) {
-			chai
-				.request(server)
-				.post('/api/issues/test')
-				.send({
-					assigned_to: 'Frodo',
-					status_text: 'High Priority'
-				})
-				.end(function(err, res) {
-					assert.equal(res.status, 200);
-					assert.equal(res.type, 'application/json', 'Response should be json');
-					assert.equal(res.body.error, 'You have not completed the form.');
-					done();
-				});
-		});
-	});
+    test('Missing required fields', function(done) {
+      chai
+        .request(server)
+        .post('/api/issues/test')
+        .send({
+          assigned_to: 'Frodo',
+          status_text: 'High Priority'
+        })
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json', 'Response should be json');
+          assert.equal(res.body.error, 'You have not completed the form.');
+          done();
+        });
+    });
+  });
 
-	suite('PUT /api/issues/{project} => text', function() {
-		test('No body', function(done) {});
+  suite('PUT /api/issues/{project} => text', function() {
+    test('No body', function(done) {
+      chai
+        .request(server)
+        .put('/api/issues/test')
+        .send({ _id: '5ce99ae14dc63b7b5f25edaa' })
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'text/html', 'Response should be text');
+          assert.equal(res.text, 'no updated field sent');
+          done();
+        });
+    });
 
-		test('One field to update', function(done) {});
+    test('One field to update', function(done) {
+      chai
+        .request(server)
+        .put('/api/issues/test')
+        .send({
+          _id: '5ce99ae14dc63b7b5f25edaa',
+          issue_title: 'new title'
+        })
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'text/html', 'Response should be text');
+          assert.equal(res.text, 'successfully updated');
+          done();
+        });
+    });
 
-		test('Multiple fields to update', function(done) {});
-	});
+    test('Multiple fields to update', function(done) {
+      chai
+        .request(server)
+        .put('/api/issues/test')
+        .send({
+          _id: '5ce99ae14dc63b7b5f25edaa',
+          issue_title: 'new title',
+          issue_text: 'new issue text'
+        })
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'text/html', 'Response should be text');
+          assert.equal(res.text, 'successfully updated');
+          done();
+        });
+    });
+  });
 
-	suite(
-		'GET /api/issues/{project} => Array of objects with issue data',
-		function() {
-			test('No filter', function(done) {
-				// prettier-ignore
-				chai.request(server)
+  suite(
+    'GET /api/issues/{project} => Array of objects with issue data',
+    function() {
+      test('No filter', function(done) {
+        chai
+          .request(server)
           .get('/api/issues/test')
           .query({})
           .end(function(err, res) {
@@ -105,17 +145,17 @@ suite('Functional Tests', function() {
             assert.property(res.body[0], '_id');
             done();
           });
-			});
+      });
 
-			test('One filter', function(done) {});
+      test('One filter', function(done) {});
 
-			test('Multiple filters (test for multiple fields you know will be in the db for a return)', function(done) {});
-		}
-	);
+      test('Multiple filters (test for multiple fields you know will be in the db for a return)', function(done) {});
+    }
+  );
 
-	suite('DELETE /api/issues/{project} => text', function() {
-		test('No _id', function(done) {});
+  suite('DELETE /api/issues/{project} => text', function() {
+    test('No _id', function(done) {});
 
-		test('Valid _id', function(done) {});
-	});
+    test('Valid _id', function(done) {});
+  });
 });
